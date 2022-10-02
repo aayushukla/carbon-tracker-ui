8cbb63d0b74f950c47554bfffb065984e352f398
Jyothirmai Kottu <jyothirmaikottu@gmail.com>
2022-10-02 10:05:36 -0700

    TotalCO2Changes, updates co2 with serialNo

---
+++ MotorService.js
@@ -0,0 +1,37 @@
+import ConnectionService from './ConnectionService';
+
+const getEntity = ConnectionService();
+
+class MotorService {
+
+    async getMotorData() {
+        const response = await getEntity.motor.list();
+
+        console.log("Fetched Motor Data:", response.items);
+
+        return response.items;
+    }
+
+
+    async getMotorDataByID (serialNumber){
+
+        const motorData = await getEntity.motor.list(
+         {
+             filter: {
+                 SerialNumber: {
+                   contains: serialNumber,
+                 },
+            }
+         
+         }
+        );
+ 
+         console.log("ID:", motorData.items);
+ 
+         return motorData.items;
+     }
+     
+}
+
+
+export default new MotorService();
