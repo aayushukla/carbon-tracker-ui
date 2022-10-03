c468022f0f09aa859a6f5db9dbb8773174a26dec
Jason Phillips <62190329+JasonTPhillipsJr@users.noreply.github.com>
2022-10-02 18:10:04 -0700

    Add files via upload
    Hornet Power Tool Service. 
    Selects the power tool in the database based off the serial number inputted in TotalCO2Component.js

--- src/services/HPTService.js
+++ src/services/HPTService.js
@@ -1,36 +1,41 @@
 import ConnectionService from './ConnectionService';
-
 const getEntity = ConnectionService();
 
 class HPTService {
 
-    async getHPTData() {
+    async getHornetPowerToolData() {
         const response = await getEntity.hornetPowerTool.list();
 
-        console.log("Fetched Motor Data:", response.items);
+        console.log("Fetched HPT Data:", response.items);
 
         return response.items;
     }
 
+    async getHornetPowerToolDataByID (serialNumber){
 
-    async getHPTDataByID (serialNumber){
-
-        const hptData = await getEntity.hornetPowerTool.list(
-         {
+        const hornetPowerToolData = await getEntity.hornetPowerTool.list(
+            {
              filter: {
-                serialNumber: {
+                 serialNumber: {
                    contains: serialNumber,
                  },
             }
-         
-         }
+        }
         );
- 
-         console.log("ID:", hptData.items);
- 
-         return hptData.items;
+
+         //Displays Hornet Power Tool as a list of attributes.
+         console.log("HPT Items",hornetPowerToolData.items)
+         //Displays Hornet Power Tool Serial Number.
+         console.log("HPT ID:", hornetPowerToolData.items[0].serialNumber);
+         //Displays Serial Number for the motor used in HPT.
+         console.log("HPT Moter Used", hornetPowerToolData.items[0].motorUsed);
+         //Displays Serial Number for the batter used in HPT.
+         console.log("HPT Battery Used", hornetPowerToolData.items[0].batteryUsed);
+
+         //Returns the entire list of attributes for the HPT with serialNumber
+         //From this values we can pull all other part data.
+         return hornetPowerToolData.items;
      }
-     
 }
 
 
