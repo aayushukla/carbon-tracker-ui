70e9185475cc072c9630d20f3ac697240fb46d62
Jason Phillips <62190329+JasonTPhillipsJr@users.noreply.github.com>
2022-10-04 10:34:05 -0700

    Add files via upload
    updated service with updated key

--- src/services/ConnectionService.js
+++ src/services/ConnectionService.js
@@ -3,8 +3,8 @@ import { createVendiaClient } from '@vendia/client';
 function ConnectionService() {
     const client = createVendiaClient({
         apiUrl: 'https://x1bd2hufjf.execute-api.us-west-1.amazonaws.com/graphql/',
-        webSocketUrl: 'wss://xjom57x8q7.execute-api.us-west-1.amazonaws.com/graphql/',
-        apiKey: 'GsPwFcx4G8iFzKEDX8U3wACBudPXccdsLXmJ7HEqCuNx'
+        webSocketUrl: 'wss://xjom57x8q7.execute-api.us-west-1.amazonaws.com/graphql',
+        apiKey: '9e3tWsCCtySg1RmsirMfdQf7mLaYYK7wLqxLEkpjJABN'
     });
 
     const { entities } = client;
