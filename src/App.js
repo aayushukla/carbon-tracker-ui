6a9d4007920c0cb93ae4c02c35d9e62316e7425e
Aayush Shukla <aayush@Ayushs-MacBook-Pro.local>
2022-10-20 12:34:04 -0700

    added corrections for app.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,9 @@ import ViewMotorComponent from "./components/motor/ViewMotorComponent"
 import BatteryComponent from "./components/BatteryComponent"
 import AddBatteryComponent from "./components/battery/AddBatteryComponent"
 import ViewBatteryComponent from "./components/battery/ViewBatteryComponent"
+import SeaRouteComponent from "./components/SeaRoute"
+import RoadComponent from "./components/RoadRoute"
+import RoadRouteComponent from "./components/road/ViewRoadComponent"
 
 
 function App() {
@@ -34,8 +37,8 @@ function App() {
           <Route path="/battery" element={<BatteryComponent />} />
           <Route path="/addBattery" element={<AddBatteryComponent />} />
           <Route path="/viewBattery" element={<ViewBatteryComponent />} />
-          <Route path="/roadRoute" element={<RoadRouteComponent />} />
-          <Route path="/viewRoad" element={<ViewRoadComponent />} />
+          <Route path="/roadRoute" element={<RoadComponent />} />
+          <Route path="/viewRoad" element={<RoadRouteComponent />} />
           <Route path="/seaRoute" element={<SeaRouteComponent />} />          
         </Routes>
       </BrowserRouter>
