dc78ce8a984a51e27a29e5691f00c5865f2a1ce0
Jyothirmai Kottu <jyothirmaikottu@gmail.com>
2022-12-08 00:41:56 -0800

    loader changes

--- src/components/motor/AddMotorComponent.js
+++ src/components/motor/AddMotorComponent.js
@@ -4,7 +4,7 @@ import MotorService from '../../services/MotorService';
 import CO2NavBar from '../CO2NavBar';
 import SidebarComponent from '../SidebarComponent';
 import { MDBFile } from 'mdb-react-ui-kit';
-import loader from '/Users/jkottu/CSC234/carbon-project/carbon-tracker-ui/src/components/gidForUpdate.gif'
+import loader from '../../../public/gifForUpdate.gif'
 
 
 
