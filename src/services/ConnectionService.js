dd0e2840497b6fcacbb1f356db19a796a5f9a061
sujatacsus50 <113576397+sujatacsus50@users.noreply.github.com>
2022-12-07 19:29:42 -0800

    Update API key

--- src/services/ConnectionService.js
+++ src/services/ConnectionService.js
@@ -4,7 +4,7 @@ function ConnectionService() {
     const client = createVendiaClient({
         apiUrl: 'https://x1bd2hufjf.execute-api.us-west-1.amazonaws.com/graphql/',
         webSocketUrl: 'wss://xjom57x8q7.execute-api.us-west-1.amazonaws.com/graphql',
-        apiKey: '83y4dnWRhjawz2ubnZY2dCWhqHVLNREP7n1XnMoccyGb'
+        apiKey: '8PLVX9hRNwHurrnPBCcCYuVc6NVxVsHMhTksdzVzDZjg'
     });
 
     const { entities } = client;
